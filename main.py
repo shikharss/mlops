f13de3f5ea3fc6dc99d42728d40d94e004a412a8
Shikhar Srivastava <ssrivast098@gmail.com>
2020-05-28 12:39:04 +0530

    new

--- main.py
+++ main.py
@@ -1,37 +1,61 @@
 import keras
-from keras.datasets import mnist
+from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
-from keras.utils.np_utils import to_categorical
-from keras.layers import Dense
-from keras.optimizers import Adam
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Conv2D, MaxPooling2D, ZeroPadding2D
+from keras.layers.normalization import BatchNormalization
+from keras.regularizers import l2
+from keras.datasets import mnist
+from keras.utils import np_utils as npu
 from keras.backend import clear_session
-import numpy
 
+(x_train, y_train), (x_test, y_test)  = mnist.load_data()
+x_train.shape
 
-# Load Model 
-(train_X , train_y), (test_X , test_y) = mnist.load_data("mymnist.data")
-# Reshape data and change type
-test_X = test_X.reshape(-1 , 28*28)
-train_X = train_X.reshape(-1 ,  28*28)
-test_X = test_X.astype("float32")
-train_X = train_X.astype("float32")
-# One hot encoding 
-test_y = to_categorical(test_y)
-train_y = to_categorical(train_y)
+img = x_train[0].shape
+print("size of image",img)
+x_train = x_train.reshape(-1, 28, 28, 1)
+x_test = x_test.reshape(-1, 28,28 , 1)
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+y_train = npu.to_categorical(y_train)
+y_test = npu.to_categorical(y_test)
 
-# Add layers to model
 model = Sequential()
-model.add(Dense(units=200 , input_dim = 28*28 , activation = 'relu'))
-model.add(Dense(units=60 , input_dim = 28*28 , activation = 'relu'))
-model.add(Dense(units = 20 , input_dim = 28*28 , activation = 'relu'))
-model.add(Dense(units=10 , input_dim = 28*28 , activation = 'softmax'))
-model.compile( optimizer= "Adam" , loss='categorical_crossentropy', metrics=['accuracy'] )
-fit_model = model.fit(train_X ,  train_y , epochs = 2 , verbose =  False)
 
-text = fit_model.history
-accuracy = text['accuracy'][1] * 100
-accuracy = int(accuracy)
+kernel = 8
+filter = 3
+pool = 2
+
+
+model.add(Conv2D(kernel, (filter,filter), input_shape = (28, 28, 1), activation = 'relu'))
+model.add(MaxPooling2D(pool_size =(pool,pool)))
+
+model.add(Flatten())
+
+model.add(Dense(10, activation = 'relu'))
+model.add(Dense(10, activation = 'softmax'))
+model.compile( optimizer= "Adam" , loss='categorical_crossentropy',  metrics=['accuracy'] )
+print(model.summary())
+
+batch_size = 128
+epochs = 1
+
+history = model.fit(x_train, y_train,
+          batch_size=batch_size,verbose=1,
+          epochs=epochs,
+          validation_data=(x_test, y_test),
+          shuffle=True)
+
+model.save("mnist_LeNet.h5")
+
+# Evaluate the performance of our trained model
+scores = model.evaluate(x_test, y_test, verbose=False)
+print('Test loss:', scores[0])
+print('Test accuracy:', scores[1])
+accuracy = scores[1]
+
 f= open("accuracy.txt","w+")
 f.write(str(accuracy))
 f.close()
-print("Accuracy of the model is : " , accuracy ,"%")
+print("Accuracy is : " , accuracy ,"%")
